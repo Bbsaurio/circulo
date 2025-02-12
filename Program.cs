7ce2478b387b3c32afec7e270bc3ea9f1b60dd53
sosag <sosag@GAEL>
2025-02-12 16:30:34 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circulo
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Ingresa el radio");
+            double r=double.Parse(Console.ReadLine());
+            System.Console.WriteLine("Ingresa pi");
+            double p= double.Parse(Console.ReadLine());
+            double A = (r * r) * p; 
+            System.Console.WriteLine("El resultado es:" +A);
+            Console.ReadKey(); 
+        }
+    }
+}
